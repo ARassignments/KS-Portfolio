01116fb933868a6d3f3e036a133ebd94a9de7cf0
AR Assignments <49037725+ARassignments@users.noreply.github.com>
2024-10-24 23:47:16 +0500

    upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,6 +10,29 @@ document.onreadystatechange = function () {
   }
 };
 
+// Function to highlight the link corresponding to the target section
+function highlightLink() {
+  // Remove 'active' class from all links
+  document.querySelectorAll('header .links a:not(.logo)').forEach(link => {
+    link.classList.remove('active');
+  });
+
+  // Get the current target section
+  const targetSection = document.querySelector('section:target');
+  
+  if (targetSection) {
+    // Find the link that corresponds to the targeted section
+    const targetLink = document.querySelector(`header .links a[href="index.html#${targetSection.id}"]:not(.logo)`);
+    if (targetLink) {
+      targetLink.classList.add('active');
+    }
+  }
+}
+
+// Run the function when the page is loaded and when the hash changes
+window.addEventListener('hashchange', highlightLink);
+window.addEventListener('load', highlightLink);
+
 if (document.querySelector(".floatingAction")) {
   window.onscroll = () => {
       if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
@@ -29,28 +52,70 @@ if (document.querySelector(".floatingAction")) {
   }
 }
 
-if (document.querySelector(".portfolioSection #portfolioContainer")) {
-  document.querySelector(".portfolioSection #portfolioContainer").innerHTML =
-    "";
-  for (let i = 0; i < projects.length; i++) {
-    document.querySelector(
-      ".portfolioSection #portfolioContainer"
-    ).innerHTML += `
-            <div class="col-lg-4 col-md-6 mb-2">
-                <a class="card"
-                    target="_blank" onclick="modalContentSet('${
-                      projects[i].pName
-                    }','${projects[i].pImage}','${projects[i].pLink}','${
-      projects[i].pDesc
-    }','${JSON.stringify(projects[i].pTech)}')">
-                    <img src="assets/images/projects/${projects[i].pImage}" alt>
-                    <div class="overlay" data-category="${projects[i].pCategory}" data-name="${projects[i].pName}"><ion-icon
-                            name="add-outline"></ion-icon></div>
-                </a>
-            </div>
-        `;
+if(document.querySelector(".portfolioSection")){
+  for (let i = 0; i < categories.length; i++) {
+    document.querySelector(".portfolioSection .filters").innerHTML += `
+      <button class="nav-link" onclick="filterData('${categories[i]}')" type="button" data-bs-toggle="pill">${categories[i]}</button>
+    `;
+    
   }
 }
+
+filterData("all");
+
+function filterData(filter){
+  if(filter == "all"){
+    if (document.querySelector(".portfolioSection #portfolioContainer")) {
+      document.querySelector(".portfolioSection #portfolioContainer").innerHTML =
+        "";
+      for (let i = 0; i < projects.length; i++) {
+        document.querySelector(
+          ".portfolioSection #portfolioContainer"
+        ).innerHTML += `
+                <div class="col-lg-4 col-md-6 mb-2">
+                    <a class="card"
+                        target="_blank" data-name="${projects[i].pName}" data-category="${projects[i].pCategory}" onclick="modalContentSet('${
+                          projects[i].pName
+                        }','${projects[i].pImage}','${projects[i].pLink}','${
+          projects[i].pDesc
+        }','${JSON.stringify(projects[i].pTech)}')">
+                        <img src="assets/images/projects/${projects[i].pImage}" alt>
+                        <div class="overlay" data-category="${projects[i].pCategory}" data-name="${projects[i].pName}"><ion-icon
+                                name="add-outline"></ion-icon></div>
+                    </a>
+                </div>
+            `;
+      }
+    }
+  } else {
+    if (document.querySelector(".portfolioSection #portfolioContainer")) {
+      document.querySelector(".portfolioSection #portfolioContainer").innerHTML =
+        "";
+      for (let i = 0; i < projects.length; i++) {
+        if(projects[i].pCategory === filter){
+          document.querySelector(
+            ".portfolioSection #portfolioContainer"
+          ).innerHTML += `
+                  <div class="col-lg-4 col-md-6 mb-2">
+                      <a class="card"
+                          target="_blank" data-name="${projects[i].pName}" data-category="${projects[i].pCategory}" onclick="modalContentSet('${
+                            projects[i].pName
+                          }','${projects[i].pImage}','${projects[i].pLink}','${
+            projects[i].pDesc
+          }','${JSON.stringify(projects[i].pTech)}')">
+                          <img src="assets/images/projects/${projects[i].pImage}" alt>
+                          <div class="overlay" data-category="${projects[i].pCategory}" data-name="${projects[i].pName}"><ion-icon
+                                  name="add-outline"></ion-icon></div>
+                      </a>
+                  </div>
+              `;
+        }
+      }
+    }
+  }
+}
+
+
 
 function modalContentSet(pName, pImage, pLink, pDesc, pTech) {
   document.querySelector(".modalSection").classList.toggle("active");
